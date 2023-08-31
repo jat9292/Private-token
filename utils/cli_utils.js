fb04b86e805c856919609d1155c0d3e4121e7fa2
Joseph-André Turk <josephandre.turk@gmail.com>
2023-08-31 13:24:27 +0200

    added fix to use genProof via cli_utils.js also
    Former-commit-id: f226eef980c36e0fbf104b81002dee370e13209d
    Former-commit-id: 6b9a5c60898a07366b166852c7f9701d5f3cb84b
    Former-commit-id: 4542083e86febfca9a1c83246908f861ed19186f
    Former-commit-id: a4c3afb62054e46fb43b46ec51c7ae1053ee86dc

--- utils/cli_utils.js
+++ utils/cli_utils.js
@@ -1,7 +1,7 @@
 import * as myFunctions from './babyjubjub_utils.js';
-import * as myFunctions2 from './babygiant_utils.js';
+import * as myFunctions2 from './proof_utils.js';
 
-const main = () => {
+const main = async () => {
     const functionName = process.argv[2];
     const args = process.argv.slice(3);
 
@@ -9,7 +9,7 @@ const main = () => {
         console.log(myFunctions[functionName](...args));
     } else {
         if (myFunctions2[functionName]) {
-            console.log(myFunctions2[functionName](...args));
+            console.log(await myFunctions2[functionName](...args));
         } else{
         console.error('Function not found!');
         }
