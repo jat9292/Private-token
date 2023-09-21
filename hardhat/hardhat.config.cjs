2b6c9ec22923011ce44fad746a4ac8ecd80b80a7
Joseph-André Turk <josephandre.turk@gmail.com>
2023-09-21 23:13:16 +0200

    Fixed minor error in hardhat config

--- hardhat/hardhat.config.cjs
+++ hardhat/hardhat.config.cjs
@@ -6,6 +6,7 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
   solidity: {
     version: "0.8.19",
     settings: {
@@ -15,18 +16,19 @@ module.exports = {
       },
     },
   },
+  
   networks: {
     hardhat: {
       chainId: 31337,
     },
     sepolia: {
       chainId: 11155111,
-      url: process.env.SEPOLIA_RPC_URL,
-      accounts: [process.env.TESTING_PRIVATE_KEY],
+      url: process.env.SEPOLIA_RPC_URL||"",
+      accounts: [process.env.TESTING_PRIVATE_KEY||'0'.repeat(64)],
       saveDeployments: true,
       verify: {
         etherscan: {
-          apiKey: process.env.ETHERSCAN_API_KEY
+          apiKey: process.env.ETHERSCAN_API_KEY||""
         }
       }
   },
@@ -36,7 +38,7 @@ module.exports = {
     deployer: {
       default: 0, // here this will by default take the first account as deployer
       1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
-      sepolia: "0xF273ac6D39fe8FBa3FA49e5085dB6411B1FeE943", //it can also specify a specific netwotk name (specified in hardhat.config.js)
+      sepolia: process.env.TESTING_PUBLIC_KEY||"", //it can also specify a specific netwotk name (specified in hardhat.config.js)
       31337: 0,
     },
   },
